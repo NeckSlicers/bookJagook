663c92f8d6eaeb8eb4e000aa24188a56760a74cf
YHJ96 <yhj878812@naver.com>
2022-12-01 18:16:02 +0900

    feat: Home & WishList & MyBooks & Mypage 추가 완료

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,1 +1,6 @@
-export {};
+import Home from './Home';
+import WishList from './WishList';
+import MyBooks from './MyBooks';
+import Mypage from './Mypage';
+
+export { Home, WishList, MyBooks, Mypage };
