cd5241b4c209be80b07c04dc845e3b69d54ba8da
YHJ96 <yhj878812@naver.com>
2022-12-01 18:10:22 +0900

    feat: 기본 라우팅 설정 완료

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home, WishList, MyBooks, Mypage } from './pages';
+import { PATH } from './constants';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path={PATH.HOME} element={<Home />} />
+      <Route path={PATH.WISHLIST} element={<WishList />} />
+      <Route path={PATH.MYBOOKS} element={<MyBooks />} />
+      <Route path={PATH.MYPAGE} element={<Mypage />} />
+    </Routes>
+  );
 }
 
 export default App;
