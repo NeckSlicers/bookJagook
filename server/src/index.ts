dd4ba64948d08e1923928e4fc8f1cbb569b7af51
YHJ96 <yhj878812@naver.com>
2022-12-02 18:27:03 +0900

    feat: 몽고DB 연결완료

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,11 @@
 import exrpess from 'express';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const PORT = 8080;
 const app = exrpess();
+dotenv.config();
 
 app.use(morgan('tiny'));
 app.use(exrpess.urlencoded({ extended: false }));
@@ -12,7 +15,9 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   // eslint-disable-next-line no-console
+  const response = await mongoose.connect(process.env.MOGODB_KEY as string);
+  if (response) console.log('DB Connent');
   console.log(`http://localhost:${PORT}`);
 });
