c81eb8bcb92b1e367786efae87be9cdd190cb41a
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-12-03 15:07:46 +0900

    design: pages/MyBooks 나의 서재 페이지 메인 그리드 레이아웃 구현

---
+++ client/src/pages/MyBooks/Books/index.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { AiOutlineStar } from 'react-icons/ai';
+import {
+  BookCards,
+  BookCard,
+  BookImg,
+  BookInfo,
+  BookTitle,
+  BookRate,
+} from './styles.Books';
+
+function Books() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModalHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    // TODO : isOpen의 상태를 변경하는 메소드를 구현합니다.
+    e.stopPropagation();
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <BookCards>
+      <BookCard>
+        <BookImg
+          src="https://image.aladin.co.kr/product/30366/97/coversum/k422830688_2.jpg"
+          alt="book-cover"
+        />
+        <BookInfo>
+          <BookTitle>백만장자를 위한 공짜 음식 1</BookTitle>
+          <BookRate type="button" onClick={openModalHandler}>
+            <AiOutlineStar />
+            <AiOutlineStar />
+            <AiOutlineStar />
+            <AiOutlineStar />
+            <AiOutlineStar />
+          </BookRate>
+        </BookInfo>
+      </BookCard>
+    </BookCards>
+  );
+}
+
+export default Books;
