f060bfcf327717b90520949f59cd5239976064ca
YHJ96 <yhj878812@naver.com>
2022-12-04 23:43:53 +0900

    feat: 신간 책 리스트, 베스트 셀러 책 리스트 API 컨트롤러 구현 완료

---
+++ server/src/controllers/bookListController.ts
@@ -0,0 +1,116 @@
+import { Request, Response } from 'express';
+import { axios, REQUEST } from '../api';
+
+interface AladinBook {
+  isbn13: string;
+  cover: string;
+  link: string;
+  title: string;
+  author: string;
+  description: string;
+  publisher: string;
+  priceStandard: number;
+  priceSales: number;
+}
+
+interface BookItem {
+  id: string;
+  bookImg: string;
+  link: string;
+  title: string;
+  author: string;
+  description: string;
+  publisher: string;
+  priceStandard: number;
+  priceSales: number;
+}
+
+interface IBookList {
+  totalPage: number;
+  page: number;
+  resultItem: number;
+  items?: BookItem[];
+}
+
+const getNewBookList = async (page: string, resultItem: string) => {
+  const { data } = await axios.get(
+    REQUEST.NEW_BOOK_LIST(Number(page), Number(resultItem)),
+  );
+
+  const newItems = data.item.map((item: AladinBook) => ({
+    id: item.isbn13,
+    bookImg: item.cover,
+    link: item.link,
+    title: item.title,
+    author: item.author,
+    description: item.description,
+    publisher: item.publisher,
+    priceStandard: item.priceStandard,
+    priceSales: item.priceSales,
+  }));
+
+  const result: IBookList = {
+    totalPage: data.totalResults / Number(resultItem),
+    page: Number(page),
+    resultItem: Number(page),
+    items: newItems,
+  };
+
+  return result;
+};
+
+const getBestsellerBookList = async (page: string, resultItem: string) => {
+  const { data } = await axios.get(
+    REQUEST.BEST_SELLER_LIST(Number(page), Number(resultItem)),
+  );
+
+  const newItems = data.item.map((item: AladinBook) => ({
+    id: item.isbn13,
+    bookImg: item.cover,
+    link: item.link,
+    title: item.title,
+    author: item.author,
+    description: item.description,
+    publisher: item.publisher,
+    priceStandard: item.priceStandard,
+    priceSales: item.priceSales,
+  }));
+
+  const result: IBookList = {
+    totalPage: data.totalResults / Number(resultItem),
+    page: Number(page),
+    resultItem: Number(page),
+    items: newItems,
+  };
+
+  return result;
+};
+
+const getBookList = async (req: Request, res: Response) => {
+  const { type = 'newbooks', page = '1', resultItem = '10' } = req.query;
+
+  try {
+    if (type === 'newbooks') {
+      const result = await getNewBookList(page as string, resultItem as string);
+      return res.status(200).json(result);
+    }
+
+    if (type === 'bestseller') {
+      const result = await getBestsellerBookList(
+        page as string,
+        resultItem as string,
+      );
+      return res.status(200).json(result);
+    }
+  } catch {
+    return res.status(500).send('서버를 확인해주세요.');
+  }
+
+  return res.status(404).send('잘못된 요청이 존재합니다.');
+};
+
+const bookListController = {
+  getBookList,
+};
+
+export { bookListController };
