f65406c8c0e0d9658a8d808626603b45085b3bf1
YHJ96 <yhj878812@naver.com>
2022-12-04 22:56:26 +0900

    feat: API 경로 추가 완료

--- server/src/api/path.ts
+++ server/src/api/path.ts
@@ -1,6 +1,22 @@
 const PATH = {
   ROOT: '/',
   USER: '/user',
+  BOOKLIST: {
+    ROOT: '/bookList',
+  },
+  BOOK: {
+    ROOT: '/book',
+    BOOKID: '/:bookId',
+    REVIEW: '/:bookId/review',
+  },
+  MYBOOKLIST: {
+    ROOT: '/mybookList',
+  },
+  MYBOOK: {
+    ROOT: '/mybook',
+    BOOKID: '/:bookId',
+    REPORT: '/:bookId/report',
+  },
 };
 
 export { PATH };
