eb363213e2a64c7e47abcfa93dd33a38b3695402
YHJ96 <yhj878812@naver.com>
2022-12-04 22:57:16 +0900

    feat: 각 페이지 Router 프로퍼티 추가 완료

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,3 +1,13 @@
 import { userRouter } from './user';
+import { bookListRouter } from './bookList';
+import { bookRouter } from './book';
+import { mybookListRouter } from './mybookList';
+import { mybookRouter } from './mybook';
 
-export { userRouter };
+export {
+  userRouter,
+  bookListRouter,
+  bookRouter,
+  mybookListRouter,
+  mybookRouter,
+};
