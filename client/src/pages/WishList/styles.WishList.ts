22814d57da7ad0c265befaef279036a127d0b589
cleats01 <cleats01@gmail.com>
2022-12-05 16:24:43 +0900

    design: 배경색 추가

--- client/src/pages/WishList/styles.WishList.ts
+++ client/src/pages/WishList/styles.WishList.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
+import { THEME } from '../../constants';
 
 export const WishListLayout = styled.div`
   padding-top: 100px;
   @media screen and (max-width: 480px) {
     padding-top: 150px;
   }
+  background-color: ${THEME.BGC};
 `;
 
 export const WishListContainer = styled.div`
