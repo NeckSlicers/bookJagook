ab8a805d33ac4ab1a9939392171e072344b09382
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-12-05 14:25:27 +0900

    feat: 마우스 호버 상태 state 추가, 마우스 enter/leave 이벤트 핸들러 구현

--- client/src/pages/MyBooks/Books/index.tsx
+++ client/src/pages/MyBooks/Books/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { AiOutlineStar } from 'react-icons/ai';
 import {
   BookCards,
@@ -13,31 +13,50 @@ import { Modal } from '../../../components';
 
 function Books() {
   const [isOpen, setIsOpen] = useState(false);
+  const [hover, setHover] = useState(false);
 
   const openModalHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-    // TODO : isOpen의 상태를 변경하는 메소드를 구현합니다.
     e.stopPropagation();
     setIsOpen(!isOpen);
   };
 
+  const hoverHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.type === 'mouseleave') return setHover(false);
+    return setHover(true);
+  };
+
   return (
     <BookCards>
-      <BookCard>
-        <BookImg
-          src="https://image.aladin.co.kr/product/30366/97/coversum/k422830688_2.jpg"
-          alt="book-cover"
-        />
+      <BookCard onMouseEnter={hoverHandler} onMouseLeave={hoverHandler}>
         <BookInfo>
-          <BookTitle>백만장자를 위한 공짜 음식 1</BookTitle>
-          <BookRate type="button" onClick={openModalHandler}>
-            <AiOutlineStar />
-            <AiOutlineStar />
-            <AiOutlineStar />
-            <AiOutlineStar />
-            <AiOutlineStar />
-          </BookRate>
+          {hover ? (
+            <>
+              <p>등록된 리뷰가 없습니다.</p>
+              <Modal modalType="review" />
+            </>
+          ) : (
+            <BookImg
+              src="https://image.aladin.co.kr/product/30366/97/coversum/k422830688_2.jpg"
+              alt="book-cover"
+            />
+          )}
         </BookInfo>
-        <Modal modalType="review" />
+        <BookTitle>백만장자를 위한 공짜 음식 1</BookTitle>
+        {/* TODO: 
+          1. 등록된 평점에 따라 별점 변경되도록 로직 추가 
+          */}
+        <BookRate type="button" onClick={openModalHandler}>
+          <AiOutlineStar />
+          <AiOutlineStar />
+          <AiOutlineStar />
+          <AiOutlineStar />
+          <AiOutlineStar />
+        </BookRate>
+        {/* TODO: 
+        1. 등록한 독후감이 있으면 "작성한 독후감 보기" 없으면 현 모달(독후감 쓰기)
+        2. 하단에 "수정", "삭제"
+        */}
+        <Modal modalType="report" />
       </BookCard>
     </BookCards>
   );
