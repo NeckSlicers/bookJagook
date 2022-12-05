6156832bdeb74cd5dd8f132d06a256c5db1a5742
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-12-05 14:31:28 +0900

    design: 별도 분리된 리뷰등록 모달 스타일링 추가

---
+++ client/src/components/molecules/Modal/ReportModal/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+/** 나의서재-독후감쓰기 전용 모달의 컨테이너 div 태그입니다.  */
+export const ReportView = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
