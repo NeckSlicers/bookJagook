54b67ea6584c5625e34bfb4b70ed965f44d4bf09
YHJ96 <yhj878812@naver.com>
2022-12-07 17:42:09 +0900

    feat: wishListController 기본 구현 완료

---
+++ server/src/controllers/wishListController.ts
@@ -0,0 +1,11 @@
+const getWishList = async () => {};
+const createWishList = async () => {};
+const deleteWishList = async () => {};
+
+const wishiListController = {
+  getWishList,
+  createWishList,
+  deleteWishList,
+};
+
+export { wishiListController };
