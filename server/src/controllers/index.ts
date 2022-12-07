942c1b3597b3f1deade61fa00285eb66d4f30968
YHJ96 <yhj878812@naver.com>
2022-12-07 17:42:22 +0900

    feat: wishList 컨트롤러 추가 완료

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,5 +1,11 @@
 import { userController } from './userController';
 import { bookListController } from './bookListController';
 import { bookController } from './bookController';
+import { wishiListController } from './wishListController';
 
-export { userController, bookListController, bookController };
+export {
+  userController,
+  bookListController,
+  bookController,
+  wishiListController,
+};
