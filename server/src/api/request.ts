fff7356b3cf0041ea5bfb092e6aa8ab651907120
YHJ96 <yhj878812@naver.com>
2022-12-07 16:37:52 +0900

    feat: 책 상세조회 주소 추가 완료

--- server/src/api/request.ts
+++ server/src/api/request.ts
@@ -3,6 +3,7 @@ const REQUEST = {
     `ItemList.aspx?QueryType=ItemNewAll&Start=${page}&MaxResults=${count}`,
   BEST_SELLER_LIST: (page: number, count: number): string =>
     `ItemList.aspx?QueryType=Bestseller&Start=${page}&MaxResults=${count}`,
+  SEARCH_BOOK: (itemId: string): string => `ItemLookUp.aspx?ItemId=${itemId}`,
 };
 
 export { REQUEST };
