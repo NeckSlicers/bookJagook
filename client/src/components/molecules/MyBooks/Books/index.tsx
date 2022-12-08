80de87dbfb255ac7d74d14fa0aa890c3957d8c65
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-12-08 15:20:17 +0900

    rename: Books, Book 컴포넌트 분리 및 pages 폴더에서 molecules 폴더 내 MyBooks 폴더로 이동

---
+++ client/src/components/molecules/MyBooks/Books/index.tsx
@@ -0,0 +1,12 @@
+import Book from './Book';
+import { BookCards } from './styles.Books';
+
+function Books() {
+  return (
+    <BookCards>
+      <Book />
+    </BookCards>
+  );
+}
+
+export default Books;
